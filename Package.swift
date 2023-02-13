d5e6740e59f8ea500bc6dd48f32b65b46d55a8c9
Satheesh Kannan <satheesh@okaythis.com>
2023-02-13 17:28:26 +0530

    Package.swift update

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "OkaySDK",
-            targets: ["PSACommon", "PSA", "Watermarker", "FccAbstractCore"]),
+            targets: ["PSACommon", "PSA", "AlteryUI", "Watermarker", "FccAbstractCore"]),
     ],
     targets: [
         .target(name: "OkaySDK"),
@@ -18,6 +18,9 @@ let package = Package(
         .binaryTarget(name: "FccAbstractCore",
                       url: "https://github.com/Okaythis/PSA_SPM/raw/4db9577f5cf0a047c43e6769bf0697df31bdd697/FccAbstractCore.xcframework.zip",
                       checksum: "8bddf32a84ba0832f2d6b7f5af274a5f416ae9a3bd72415ba448a3114201886c"),
+        .binaryTarget(name: "AlteryUI",
+                      url: "https://github.com/Okaythis/AlteryUIiOS/raw/1da76d57fc7bcb2d84f6f0439bc16ea00dabf920/AlteryUI.xcframework.zip",
+                      checksum: "87e23454f6dbea821f872d5d17d4c19796e3ac376a8de8225f8e505501535c01"),
         .binaryTarget(name: "PSA",
                       url: "https://github.com/Okaythis/PSA_SPM/raw/4db9577f5cf0a047c43e6769bf0697df31bdd697/PSA.xcframework.zip",
                       checksum: "126e67fcfd1ac75eba21c023d5f1dde148828fe10aa031fe8baa9fbeab8a30aa"),
